a2e95b519da0d059195ee8a72a81a5f1724c8016
Syn Zhang <zeyanzhang.china@gmail.com>
2014-12-28 23:34:43 +0800

    Add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contact" do
+    get :about
+    assert_respone :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
