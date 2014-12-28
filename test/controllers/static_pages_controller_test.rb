a02c2d756acbe281558ab124c9e784523695c398
Syn Zhang <zeyanzhang.china@gmail.com>
2014-12-28 23:02:25 +0800

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Chirp"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Chirp"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Chirp"
+  end
 end
