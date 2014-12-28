894f7ab844b389bc0c4b6b135e7d5cb4d8f65dfa
Syn Zhang <zeyanzhang.china@gmail.com>
2014-12-28 23:30:46 +0800

    Save main page title in a variable

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Chirp"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Chirp"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Chirp"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Chirp"
+    assert_select "title", "About | #{@base_title}"
   end
 end
