d1eea8c61dcb09b6dc96fe9f056ae3f36f585626
Syn Zhang <zeyanzhang.china@gmail.com>
2015-01-20 01:23:48 +0800

    Add password resets

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get 'login'   => 'sessions#new'
   post 'login'  => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get 'password_resets/new'
+  get 'password_resets/edit'
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name:  "Syn Zhang",
              email: "zeyanzhang.china@gmail.com",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(name:  "Syn Zhang",
   User.create!(name: name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
