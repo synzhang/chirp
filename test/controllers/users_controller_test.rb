a87e2c288db82b3260d7b05f1240f32882b6fa21
Syn Zhang <zeyanzhang.china@gmail.com>
2015-01-27 01:50:20 +0800

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -52,4 +52,14 @@ class UsersControllerTest < ActionController::TestCase
     end
     assert_redirected_to root_url
   end
+
+  test "should redirect following when not logged in" do
+    get :following, @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, @user
+    assert_redirected_to login_url
+  end
 end
