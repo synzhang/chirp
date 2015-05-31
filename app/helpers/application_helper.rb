00d502a68f4faad357cb5c9662f8e6b9aaee6a62
Syn Zhang <zeyanzhang.china@gmail.com>
2015-05-31 11:42:49 +0800

    Add signed in & out class on <html>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
 
+  def page_class
+    ["#{controller_path.gsub('/', '_')}_#{action_name}",
+      content_for(:page_class),
+      if logged_in?
+        "signed_in"
+      else
+        "signed_out"
+      end
+    ].reject(&:blank?).join(' ')
+  end
+
   def full_title(page_title = '')
     base_title = "Chirp"
     if page_title.empty?
